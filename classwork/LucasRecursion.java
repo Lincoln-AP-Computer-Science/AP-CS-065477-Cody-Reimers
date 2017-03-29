9219f1cd31818bf1164480684e37a3927e06bcaa
Cody Reimers <r2thehorde2@yahoo.com>
2017-03-29 09:26:15 -0700

    Add files via upload

---
+++ classwork/LucasRecursion.java
@@ -0,0 +1,26 @@
+public class LucasRecursion
+{
+   
+   public static void main(String[] args)
+   {
+      System.out.print("Lucas Numbers are like Fibonacci Numbers,\nbut with a difference:");
+      System.out.println("the Lucas Numbers\nstart with 2, 1, and 3 instead of 0, 1, and 1.");
+      System.out.println("The 6th Lucas Number is: " + lucas(6));
+      System.out.println("The 9th Lucas Number is: " + lucas(9));
+      System.out.println("The 15th Lucas Number is: " + lucas(15));
+      System.out.println("The 23rd Lucas Number is: " + lucas(23));
+   }
+   
+   public static int lucas(int n)
+   {
+      if (n == 0)
+         return 2;
+      else if (n == 1)
+         return 1;
+      else if (n == 2)
+         return 3;
+      else
+         return lucas(n - 1) + lucas(n - 2);
+   }
+   
+}
